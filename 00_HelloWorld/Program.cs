bf64cf536cb1cd03f16b00c857e80196c94d3572
Lucas Coxe <lucascoxe@Lucass-MacBook-Air.local>
2022-02-01 11:45:01 -0500

    initial commit

---
+++ 00_HelloWorld/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("I am super hungry right now");
+Console.WriteLine("My name is Lucas");
+Console.WriteLine("I am 18 years old");
+Console.WriteLine("I will eat sushi for lunch");
+Console.WriteLine("I might just have to get some tea from Starbucks with it.")
