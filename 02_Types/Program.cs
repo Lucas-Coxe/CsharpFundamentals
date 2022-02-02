5a64f109563946831bd01fddae682b9f0756af81
Lucas Coxe <lucascoxe@Lucass-MacBook-Air.local>
2022-02-02 11:12:35 -0500

    Adding 02_Types to Fundamentals sln/project

---
+++ 02_Types/Program.cs
@@ -0,0 +1,14 @@
+﻿bool isLoggedIn = true;
+Console.WriteLine( "Login Status: " + isLoggedIn);
+
+int age = 42;
+Console.WriteLine("age: " + age);
+
+double balanceDue = 800.99d;
+Console.WriteLine("Balance Due " + balanceDue);
+
+decimal accountBalance = 9000.02m;
+Console.WriteLine("Balance: " + accountBalance);
+
+float averageHeight = 5.7f;
+Console.WriteLine("Height " + averageHeight);
