5a068a58862a735c0e8bd9e902a961272eb03289
Lucas Coxe <lucascoxe@Lucass-Air.askey.com>
2022-02-02 12:24:34 -0500

    adding 04_strings to Fundamentals sln/project

---
+++ 04_Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿string first = "The cars we sell are";
+string second = "BMW, Lexus, Subaru, Mercedes, and Tesla";
+
+string concatenatedResult = first + " " + second;
+
+Console.WriteLine(concatenatedResult);
+
+string firstName = "Lucas";
+
+string lastName = "Coxe";
+
+string compositeResult = string.Format("His name is {0} {1}", firstName, lastName);
+
+Console.WriteLine(compositeResult);
+
+string interpolationResult = $"His name is still {firstName} {lastName}! ";
+Console.WriteLine(interpolationResult);
+
