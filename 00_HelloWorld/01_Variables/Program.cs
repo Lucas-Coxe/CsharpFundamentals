97e4f0bf5eab60bfc881e6f9c553577e914baa33
Lucas Coxe <lucascoxe@Lucass-MacBook-Air.local>
2022-02-02 10:49:14 -0500

    adding stuff

---
+++ 00_HelloWorld/01_Variables/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine(" Hello What is your first name?");
+string firstName = Console.ReadLine();
+Console.WriteLine(" Hi " + firstName);
+
+Console.WriteLine("What is your favorite color");
+string color = Console.ReadLine();
+
+Console.WriteLine( "Your favorite color is " + color);
+
--- 00_HelloWorld/Program.cs
+++ 00_HelloWorld/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("I am super hungry right now");
 Console.WriteLine("My name is Lucas");
 Console.WriteLine("I am 18 years old");
-Console.WriteLine("I will eat sushi for lunch");
-Console.WriteLine("I might just have to get some tea from Starbucks with it.")
+
+
