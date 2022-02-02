cfa797ec49a867f19fa6c222507cf3bf71ea74ff
Lucas Coxe <lucascoxe@Lucass-Air.askey.com>
2022-02-02 12:06:09 -0500

    Adding 03_Operators to Fundamental Sln/projects

---
+++ 03_Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿int additionResult = 5 + 2;
+Console.WriteLine("5 + 2 =  " + additionResult );
+
+int subtractionResult = 5 - 2;
+Console.WriteLine("5 - 2 = " + subtractionResult);
+
+int multiplicationResult = 5 * 2;
+Console.WriteLine("5 * 2 = " + multiplicationResult);
+
+decimal divisionResult = 5 / 2m;
+Console.WriteLine("5 / 2 = " + divisionResult);
+
+Console.WriteLine(10 % 2);
+Console.WriteLine(10 % 9);
+Console.WriteLine(16 % 3);
+Console.WriteLine(19 % 5);
+
+Console.WriteLine(10 + 10);
