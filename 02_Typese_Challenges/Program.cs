10f1c3603e544b1f627347d3d76249777cdfd100
Lucas Coxe <lucascoxe@Lucass-Air.askey.com>
2022-02-02 15:13:59 -0500

    Adding challenges to Fundamentals

---
+++ 02_Typese_Challenges/Program.cs
@@ -0,0 +1,52 @@
+﻿//Bronze challenge
+
+int number;
+int number2 = 30;
+
+string numberNames;
+string realNumberNames = "Thirty";
+bool trueFalse;
+bool thirtyBig = true;
+
+float crazyNumbers;
+float numCrazy = 685.7f;
+
+double num3;
+double num2Times = 48d;
+
+decimal dotNum;
+decimal decimalNum = 43.45m;
+
+//Unassigned longs
+ulong posNumber;
+ulong maxValue = 123468485666732;
+
+Console.WriteLine(maxValue);
+Console.WriteLine(number2);
+Console.WriteLine(numCrazy);
+
+
+
+//silver
+
+string Name1 = "John";
+string Name2 = "Steve";
+
+string words = "A String full of words. Also I am adding the number 200 to my string ";
+int integer = 200;
+
+
+Console.WriteLine(Name1 + " " + Name2);
+Console.WriteLine(words + " " + integer);
+
+
+
+//gold
+
+
+string year = "2022";
+int yearNum = int.Parse(year);
+Console.WriteLine(yearNum);
+
+int Num2 = Convert.ToInt32(year);
+Console.WriteLine(Num2);
