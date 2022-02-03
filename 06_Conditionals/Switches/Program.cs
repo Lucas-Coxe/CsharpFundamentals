69cfd6f4aa631062308789a25aa7b1f7aeb757f2
Lucas Coxe <lucascoxe@Lucass-Air.askey.com>
2022-02-03 11:19:25 -0500

    Pushing Switches to Fundamentals sln/Project

---
+++ 06_Conditionals/Switches/Program.cs
@@ -0,0 +1,38 @@
+﻿System.Console.WriteLine("Please give us your name !");
+string name = System.Console.ReadLine();
+switch(name)
+{
+    case "Lucas":
+    System.Console.WriteLine("Nice to meet you " + name);
+    break;
+    default:
+    break;
+}
+
+System.Console.WriteLine("How are you feeling today on a scale of 1-5 ");
+string feelingRating = System.Console.ReadLine();
+
+switch(feelingRating){
+
+    case "5":
+    System.Console.WriteLine("That is great to hear that! ");
+    break;
+    
+    case "4":
+    System.Console.WriteLine(" That's good!");
+    break;
+
+    case "3":
+    System.Console.WriteLine("Is everything okay? ");
+    break;
+
+    case "2":
+    System.Console.WriteLine("Sorry to hear that ");
+    break;
+
+    case "1":
+    System.Console.WriteLine("It will get better");
+    break;
+}
+
+
--- 06_Conditionals/if_statements/Program.cs
+++ 06_Conditionals/if_statements/Program.cs
@@ -3,34 +3,34 @@ bool isGoingOutside = true;
 
 if(isSnowing)
 {
-    Console.Writeline("It is snowing outside");  
+    System.Console.Writeline("It is snowing outside");  
 }
   
 if(!isSnowing || isGoingOutside)
 {
-    Console.Writeline("Better wear some snow boots !!");
+    System.Console.Writeline("Better wear some snow boots !!");
 }
 
 if(isSnowing ^ isGoingOutside)
 {
-    Console.Writeline("I will only go outside once the snow stops");
+    System.Console.Writeline("I will only go outside once the snow stops");
 }
 if(isSnowing){
-    Console.WriteLine("There is no need to shovel snow");
+    System.Console.WriteLine("There is no need to shovel snow");
 }
 
 else{
-    Console.WriteLine("I guess I need to shovel the driveway ");
+    System.Console.WriteLine("I guess I need to shovel the driveway ");
 }
 
 if(isSnowing){
-    Console.Writeline("I guess I have to go clear off the wife's car");
+    System.Console.Writeline("I guess I have to go clear off the wife's car");
 }
 
 else if(!isSnowing){
-    Console.Writeline("I don't have to clear off cars! ");
+    System.Console.Writeline("I don't have to clear off cars! ");
 }
 
 else{
-    Console.Writeline("I don't even know what we are talking about");
+    System.Console.Writeline("I don't even know what we are talking about");
     }
