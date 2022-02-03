ae75b5397bbf11bd906685fdc09e2d5ac3de949b
Lucas Coxe <lucascoxe@Lucass-Air.askey.com>
2022-02-03 10:32:18 -0500

    Adding ternaries to fundamentals sln / projects

---
+++ 06_Conditionals/Ternaries/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.Write("Enter your age ");
+string response = System.Console.ReadLine();
+int age = int.Parse(response);
+
+string output = age >= 18 ? "You can vote!!" : "You're too young to vote";
+
+System.Console.WriteLine(output);
